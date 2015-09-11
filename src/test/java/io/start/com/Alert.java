f957d4113ca1f38fe9cd719980a4510c983531c6
bchand1748 <bchand@nova.edu>
2015-09-11 16:16:29 -0400

    Signed-off-by: bchand1748 <bchand@nova.edu>

--- src/test/java/io/start/com/Alert.java
+++ src/test/java/io/start/com/Alert.java
@@ -4,23 +4,26 @@ package io.start.com;
 
 
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 
+import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Alert extends StartServer {
+public class Alert   {
 
+	public static AndroidDriver<MobileElement> driver;
 
-@BeforeTest
-public void startApp() throws MalformedURLException, InterruptedException
-{
-	 loadApp();
-}
 
 @Test
 public void checkAlert() throws InterruptedException
@@ -31,5 +34,37 @@ public void checkAlert() throws InterruptedException
 
 }
 
+@BeforeTest
+
+
+
+	
+	public void loadApp() throws MalformedURLException, InterruptedException
+		{
+		
+		 File app = new File("C:\\Users\\bkbc011\\Desktop\\apk\\production.apk");
+		 DesiredCapabilities capabilities = new DesiredCapabilities();
+		 capabilities.setCapability("automationName","appium");
+	     capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		 capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.4.2");
+		 capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+		 capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+	     driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		 driver.manage().timeouts().implicitlyWait(55, TimeUnit.SECONDS);
+		 Set<String> contextNames = driver.getContextHandles();
+	     for (String contextName : contextNames) {
+	       System.out.println(contextName);
+	       if (contextName.contains("NATIVE_APP")){
+	         driver.context(contextName);
+	        Thread.sleep(30000);
+	       }
+	      }
+			
+
+
+
+
 
 }
+}
+
